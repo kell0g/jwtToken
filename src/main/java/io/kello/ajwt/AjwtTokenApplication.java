cce1f22eb7671982fb8c158c51b089f73b9f0e9b
kelloo <kell0g2@naver.com>
2020-07-21 17:20:12 +0900

    test

---
+++ src/main/java/io/kello/ajwt/AjwtTokenApplication.java
@@ -0,0 +1,13 @@
+package io.kello.ajwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjwtTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjwtTokenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/kello/ajwt/AjwtTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kello.ajwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjwtTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
